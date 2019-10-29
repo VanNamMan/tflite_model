ec943baf818c937c26d33b640a49b83ef0a3c418
VanNamMan <manvannamk54@gmail.com>
2019-10-29 15:41:28 +0700

    tf-nighty

--- TFLite_Converter/keras2tflite.py
+++ TFLite_Converter/keras2tflite.py
@@ -1,6 +1,14 @@
 import tensorflow as tf
+from keras.models import load_model
+import sys
+
+# print(sys.path)
+
 print(tf.__version__)
 file_name = "D:/Python/Pretrained/face_net/facenet_keras.h5"
-converter = tf.lite.TFLiteConverter.from_keras_model_file(file_name)
-tflite_model = converter.convert()
-open("converted_model.tflite", "wb").write(tflite_model)
+# model = load_model(file_name)
+
+# converter = tf.contrib.lite.TFLiteConverter.from_keras_model_file(file_name)
+# converter.optimizations = [tf.contrib.lite.optimizations.]
+# tflite_model = converter.convert()
+# open("converted_model.tflite", "wb").write(tflite_model)
